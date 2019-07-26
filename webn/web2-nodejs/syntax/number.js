c06dc1cf922887c3b0593e10da90831c22fa99cf
leesiyun <siyun1221@gmail.com>
2019-07-27 00:00:06 +0900

    ajax

--- webn/web2-nodejs/syntax/number.js
+++ webn/web2-nodejs/syntax/number.js
@@ -1,4 +1,4 @@
 console.log(1 + 1);
-console.log(3 - 1);
-console.log(3 * 3);
-console.log(12 / 3);
+console.log(4 - 1);
+console.log(2 * 2);
+console.log(10 / 2);
--- webn/web2-nodejs/syntax/string.js
+++ webn/web2-nodejs/syntax/string.js
@@ -1,5 +1,5 @@
-console.log("1" + " 1");
+console.log(`1` + `1`);
 console.log(
-  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  `Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus numquam hic assumenda reprehenderit doloribus impedit suscipit velit exercitationem facilis voluptas, saepe, et rem sequi praesentium sed quaerat in perspiciatis! Repellat maiores qui pariatur consequatur. Excepturi temporibus earum laborum impedit a ipsum. Ad deserunt temporibus ratione dicta velit sequi dolor iusto!`
     .length
 );
