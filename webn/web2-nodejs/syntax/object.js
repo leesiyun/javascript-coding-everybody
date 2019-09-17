6d892e455634fd9cf01a8f674c3818d38d129504
leesiyun <siyun1221@gmail.com>
2019-09-17 18:01:18 +0900

    nodejs - object, array

---
+++ webn/web2-nodejs/syntax/object.js
@@ -0,0 +1,10 @@
+const members = ['egoing', 'k8805', 'hoya'];
+console.log(members[1]); //k8805
+
+const roles = {
+  programmer: 'egoing',
+  designer: 'k8805',
+  manager: 'hoya'
+};
+
+console.log(roles.designer); //k8805
